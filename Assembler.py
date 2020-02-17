7093afdd1c4577c4fd89671624221bc4d4bb54be
Tyler FitzGerald <45083371+tylerfitz10@users.noreply.github.com>
2020-02-17 11:01:09 -0700

    Update Assembler.py

--- Assembler.py
+++ Assembler.py
@@ -5,9 +5,12 @@ import os
 
 from address_hash_table import*
 from compute_hash_table import*
+from label_hash_table import*
 
-def symbolTable()
-
+def lHandler(command):
+    label = command.strip()
+    print(f'lHandler')
+    
 
 def aHandler(command):
     #pass @i or @sum convert to binary value
@@ -37,8 +40,6 @@ def cHandler(command):
 
     print(f'dest:{assemble["dest"]}, compt:{assemble["comp"]}, jump:{assemble["jump"]}')
 
-    #return '111' + computations[assemble['comp']] + destinations[assemble['dest']] + jumps[assemble['jump']]
-
 
 def parseCommand(command):
     # TODO Figure out which type of command and call approriate handler
@@ -46,25 +47,55 @@ def parseCommand(command):
     if command[0] == '@':
         aHandler(command)
     elif command[0] == '(':
-        print('Found L Type')
+        lHandler(command)
     else:
         cHandler(command)
 
-inFile = open(asmFile, 'r')
 
-for line in inFile:
-
-    command = line.split('//')[0].strip()
-    if command:
-
-        # todo call parseCommand function
-        parseCommand(command)
 
 if __name__ == "__main__":
-    
-asmFile = sys.argv[1]
-#TODO
-#try:
+
+    asmFile = str(sys.argv[1])
+
+    inFile = open(asmFile, 'r')
+
+    for line in inFile:
+
+        command = line.split('//')[0].strip()
+
+        if command:
+
+        # todo call parseCommand function
+            parseCommand(command)
     #asmFile
 
+
+
 inFile.close()
+"""
+lHandler
+A Type: @0
+0000000000000000
+dest:D, compt:None, jump:M
+A Type: @1
+0000000000000001
+dest:D, compt:None, jump:D-M
+A Type: @10
+0000000000001010
+dest:null, compt:D, jump:JGT
+A Type: @1
+0000000000000001
+dest:D, compt:None, jump:M
+A Type: @12
+0000000000001100
+dest:null, compt:0, jump:JMP
+A Type: @0
+0000000000000000
+dest:D, compt:None, jump:M
+A Type: @2
+0000000000000010
+dest:M, compt:None, jump:D
+A Type: @14
+0000000000001110
+dest:null, compt:0, jump:JMP
+dest:M, compt:D, jump:JMP"""
